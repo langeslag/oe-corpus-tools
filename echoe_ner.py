b3cada1f2410ab2f8e998de754f055917696f63f
Paul Langeslag <planges@uni-goettingen.de>
2026-01-19 21:56:24 +0100

    slight improvements to ner script

--- echoe_ner.py
+++ echoe_ner.py
@@ -1,6 +1,6 @@
 # This script extracts NER spellings from ECHOE.
 # It expects ECHOE at echoe/.
-# TODO: descend into <w> and nuke all spacing there.
+# TODO: abbr/am not always nuked, spaces unduly removed
 
 import re,glob,json
 from lxml import etree
@@ -10,7 +10,9 @@ equivalents = {
         '' : 's',
         'ẏ' : 'y',
         'ƿ' : 'w',
-        'ꝛ' : 'r'
+        'ꝛ' : 'r',
+        ' ': '',
+        '\n': ''
         }
 
 def multipleReplace(text, wordDict):
@@ -18,7 +20,7 @@ def multipleReplace(text, wordDict):
         text = text.replace(key, wordDict[key])
     return text
 
-parser = etree.XMLParser(remove_blank_text=False,resolve_entities=False)
+parser = etree.XMLParser(remove_blank_text=True,resolve_entities=True)
 
 forms = []
 
@@ -31,27 +33,15 @@ for file in sorted(glob.glob('echoe/xml/[0-9]*.xml')):
     root = tree.getroot()
     text = root.find('.//{http://www.tei-c.org/ns/1.0}text')
     version = root.find('.//{http://www.tei-c.org/ns/1.0}idno').text
+    delenda = ['am', 'abbr', 'lb', 'corr', 'del', 'orig']
     for category in categories:
         for element in text.iter(category):
-            if element.find('.//{http://www.tei-c.org/ns/1.0}abbr') is not None:
-                abbr = element.find('.//{http://www.tei-c.org/ns/1.0}abbr')
-                abbr.getparent().remove(abbr)
-            if element.find('.//{http://www.tei-c.org/ns/1.0}am') is not None:
-                abbr = element.find('.//{http://www.tei-c.org/ns/1.0}am')
-                abbr.getparent().remove(abbr)
-            if element.find('.//{http://www.tei-c.org/ns/1.0}lb') is not None:
-                lb = element.find('.//{http://www.tei-c.org/ns/1.0}lb')
-                lb.getparent().remove(lb)
-            if element.find('.//{http://www.tei-c.org/ns/1.0}corr') is not None:
-                corr = element.find('.//{http://www.tei-c.org/ns/1.0}corr')
-                corr.getparent().remove(corr)
-            if element.find('.//{http://www.tei-c.org/ns/1.0}del') is not None:
-                deletion = element.find('.//{http://www.tei-c.org/ns/1.0}del')
-                deletion.getparent().remove(deletion)
-            if element.find('.//{http://www.tei-c.org/ns/1.0}orig') is not None:
-                orig = element.find('.//{http://www.tei-c.org/ns/1.0}orig')
-                orig.getparent().remove(orig)
-            name = multipleReplace(etree.tostring(element, method='text', encoding='unicode').lower().lstrip().rstrip(), equivalents).replace('\n', '').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ').replace('  ', ' ')
+            for i in delenda:
+                query = './/{http://www.tei-c.org/ns/1.0}' + i
+            if element.find(query) is not None:
+                loc = element.find(query)
+                loc.getparent().remove(loc)
+            name = multipleReplace(etree.tostring(element, method='text', encoding='unicode').lower().lstrip().rstrip(), equivalents)
             print(name)
             forms.append(name)
 
